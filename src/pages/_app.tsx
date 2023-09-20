4eaaaeb1d4c09edd1a0d1ea282af13f100af1f0a
Gianluca Löwe <Gianluca.Loewe@haw-hamburg.de>
2023-09-20 16:41:56 +0200

    add sign-in and sign-up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -11,9 +11,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+      <ClerkProvider {...pageProps}>
+        {/*<SessionProvider session={session}>*/}
+          <Component {...pageProps} />
+        {/*</SessionProvider>*/}
+      </ClerkProvider>
   );
 };
 
