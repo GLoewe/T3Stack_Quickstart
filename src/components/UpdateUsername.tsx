b7e7a462d046d6c7b49b9dd1463f7378adb28f86
Gianluca Löwe <gianluca.loewe@gmail.com>
2023-11-21 17:03:20 +0100

    - fix (5)

--- src/components/UpdateUsername.tsx
+++ src/components/UpdateUsername.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const UpdateUsername:React.FC = () => {
     const [username, setUsername] = useState('');
     const {user} = useUser(); // Update this based on your user hook
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(event.target.value);
@@ -16,12 +16,12 @@ const UpdateUsername:React.FC = () => {
     };
 
 
-    // const updateUrl = () => {
-    //     if (user) {
-    //         const newPath = `/user/${username}`;
-    //         navigate(newPath);
-    //     }
-    // };
+    const updateUrl = () => {
+        if (user) {
+            const newPath = `/user/${username}`;
+            navigate(newPath);
+        }
+    };
 
     const updateUser = () => {
         if (username.length > 0 && username.length < 15) {
@@ -29,7 +29,7 @@ const UpdateUsername:React.FC = () => {
                 user?.update({
                     username: username,
                 }).catch(err => alert(err));
-                // updateUrl();
+                updateUrl();
                 setUsername("");
             } catch (error) {
                 console.error('Error updating username:', error);
